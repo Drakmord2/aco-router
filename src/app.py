5e0447340199ad4f57cf00cd725b6557a902948a
Rubens Carneiro <rubenscarneiro@midiaseducativas.com.br>
2018-06-24 23:10:06 -0300

    Google Maps API

--- src/app.py
+++ src/app.py
@@ -1,3 +1,30 @@
+import googlemaps
+from datetime import datetime
 
 def main():
-    pass
+    gmaps = googlemaps.Client(key='Add Your Key here')
+
+    # Geocoding an address
+    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+    # Look up an address with reverse geocoding
+    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+    # Request directions via public transit
+    now = datetime.now()
+    directions_result = gmaps.directions("Sydney Town Hall",
+                                         "Parramatta, NSW",
+                                         mode="transit",
+                                         departure_time=now)
+    print(directions_result)
+
+if __name__ == '__main__':
+    print('\n\t-- ACO Router --')
+
+    main()
+
+# TODO Try catch só no final
+#    try:
+#        main()
+#    except Exception as err:
+#        print('- Runtime error: ', err)
